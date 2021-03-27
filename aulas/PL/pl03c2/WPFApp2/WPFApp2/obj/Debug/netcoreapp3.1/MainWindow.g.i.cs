74cff62c12d3333e3d175c35e12b9ea453669ea6
Francisco Pereira <fsp@utad.pt>
2021-03-27 20:30:35 +0000

    Cleanup files (clean solution)

--- aulas/PL/pl03c2/WPFApp2/WPFApp2/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ aulas/PL/pl03c2/WPFApp2/WPFApp2/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -189,7 +189,7 @@ namespace WPFApp2 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPFApp2;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPFApp2;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
