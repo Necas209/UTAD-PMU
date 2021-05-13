eb164e49addc38f04fbbb06b5b728fa5b7ae003b
Francisco Pereira <fsp@utad.pt>
2021-05-14 00:41:50 +0100

    Aula PL10C9

---
+++ aulas/PL/pl10c9/WPFApp9/WPFApp9/Battery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFApp9
+{
+    public class Battery : IBattery
+    {
+        public int ChargeValue { get; set; }
+
+        public event voidArgInt ChargeChanged;
+
+        public void Charge(int delta)
+        {
+            if ((ChargeValue + delta) > 100)
+                throw new OutOfLimitsException("A bateria não suporta essa carga!!!");
+            ChargeValue += delta;
+
+            if (ChargeChanged != null)
+                ChargeChanged(ChargeValue);
+        }
+
+        public void Descharge(int delta)
+        {
+            if ((ChargeValue - delta) < 0)
+                throw new OutOfLimitsException("A bateria não suporta essa descarga!!!");
+            ChargeValue -= delta;
+
+            if (ChargeChanged != null)
+                ChargeChanged(ChargeValue);
+        }
+    }
+}
